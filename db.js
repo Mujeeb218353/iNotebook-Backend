75f2edbbe890a0ce8c24d8a78420446af162c4ce
Mujeeb218353 <139048778+Mujeeb218353@users.noreply.github.com>
2024-02-14 01:43:07 +0500

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri  = 'mongodb://127.0.0.1:27017/testinotebook';
+const uri  = 'mongodb+srv://mujeeb218353:mujeeb@786@inotebook.cakfrfe.mongodb.net/?retryWrites=true&w=majority';
 
 const connectToMongo = () =>{
     mongoose.connect(uri).then(
